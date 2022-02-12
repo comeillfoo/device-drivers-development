8a56dd7abc71ca3bdb5cfb6a68b935d724b650f6
come_ill_foo <rayveleaveme34@inbox.ru>
2022-02-12 18:31:45 +0300

    feat: stub for the lab1 and .gitignore

---
+++ lab1/src/_chr_comp.c
@@ -0,0 +1,22 @@
+#include <linux/module.h> /* essential for modules */
+#include <linux/kernel.h> /* essential for KERNEL_INFO */
+
+#include <linux/proc_fs.h> /* essential for procfs */
+#include <linux/slab.h> /* essential for kmalloc, kfree */
+
+
+MODULE_LICENSE( "Dual MIT/GPL" );
+
+#define MOD_NAME "chr_comp"
+
+static int __init init_chr_comp( void ) {
+  printk( KERN_INFO MOD_NAME ": module inited" );
+  return 0;
+}
+
+static void __exit cleanup_chr_comp( void ) {
+  printk( KERN_INFO MOD_NAME ": module cleaned up" );
+}
+
+module_init( init_chr_comp );
+module_exit( cleanup_chr_comp );
