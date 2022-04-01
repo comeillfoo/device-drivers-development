1a05c76d6bf3904097e53f0e7e3a0249dc527afc
come_ill_foo <rayveleaveme34@inbox.ru>
2022-04-01 15:07:38 +0300

    fix: started to transfer to the multi-queue queueing mechanism

--- lab2/src/_vblkdev.c
+++ lab2/src/_vblkdev.c
@@ -7,14 +7,15 @@
 #include <linux/genhd.h>
 #include <linux/blkdev.h>
 #include <linux/bio.h>
+#include <linux/blk-mq.h>
 #include <linux/string.h>
 
 
 #define MEMSIZE 0xF000 // Size of Ram disk in sectors
 int c = 0; //Variable for Major Number 
 
-#define SECTOR_SIZE 512
-#define MBR_SIZE SECTOR_SIZE
+#define MDISK_SECTOR_SIZE 512
+#define MBR_SIZE MDISK_SECTOR_SIZE
 #define MBR_DISK_SIGNATURE_OFFSET 440
 #define MBR_DISK_SIGNATURE_SIZE 4
 #define PARTITION_TABLE_OFFSET 446
@@ -23,13 +24,12 @@ int c = 0; //Variable for Major Number
 #define MBR_SIGNATURE_OFFSET 510
 #define MBR_SIGNATURE_SIZE 2
 #define MBR_SIGNATURE 0xAA55
-#define BR_SIZE SECTOR_SIZE
+#define BR_SIZE MDISK_SECTOR_SIZE
 #define BR_SIGNATURE_OFFSET 510
 #define BR_SIGNATURE_SIZE 2
 #define BR_SIGNATURE 0xAA55
 
-typedef struct
-{
+typedef struct {
 	unsigned char boot_type; // 0x00 - Inactive; 0x80 - Active (Bootable)
 	unsigned char start_head;
 	unsigned char start_sec:6;
@@ -48,16 +48,15 @@ typedef PartEntry PartTable[4];
 
 #define SEC_PER_HEAD 63
 #define HEAD_PER_CYL 255
-#define HEAD_SIZE (SEC_PER_HEAD * SECTOR_SIZE)
-#define CYL_SIZE (SEC_PER_HEAD * HEAD_PER_CYL * SECTOR_SIZE)
+#define HEAD_SIZE (SEC_PER_HEAD * MDISK_SECTOR_SIZE)
+#define CYL_SIZE (SEC_PER_HEAD * HEAD_PER_CYL * MDISK_SECTOR_SIZE)
 
-#define sec4size(s) ((((s) % CYL_SIZE) % HEAD_SIZE) / SECTOR_SIZE)
+#define sec4size(s) ((((s) % CYL_SIZE) % HEAD_SIZE) / MDISK_SECTOR_SIZE)
 #define head4size(s) (((s) % CYL_SIZE) / HEAD_SIZE)
 #define cyl4size(s) ((s) / CYL_SIZE)
 
 
-static PartTable def_part_table =
-{
+static PartTable def_part_table = {
 	{
 		boot_type: 0x00,
 		start_sec: 0x2,
@@ -84,8 +83,7 @@ static PartTable def_part_table =
 	}
 };
 static unsigned int def_log_part_br_abs_start_sector[] = {0x5000, 0xA000};
-static const PartTable def_log_part_table[] =
-{
+static const PartTable def_log_part_table[] = {
 	{
 		{
 			boot_type: 0x00,
@@ -128,41 +126,37 @@ static const PartTable def_log_part_table[] =
 	}
 };
 
-static void copy_mbr(u8 *disk)
-{
+static void copy_mbr( u8* disk ) {
 	memset(disk, 0x0, MBR_SIZE);
 	*(unsigned long *)(disk + MBR_DISK_SIGNATURE_OFFSET) = 0x36E5756D;
 	memcpy(disk + PARTITION_TABLE_OFFSET, &def_part_table, PARTITION_TABLE_SIZE);
 	*(unsigned short *)(disk + MBR_SIGNATURE_OFFSET) = MBR_SIGNATURE;
 }
-static void copy_br(u8 *disk, int abs_start_sector, const PartTable *part_table)
-{
-	disk += (abs_start_sector * SECTOR_SIZE);
+static void copy_br(u8 *disk, int abs_start_sector, const PartTable *part_table) {
+	disk += (abs_start_sector * MDISK_SECTOR_SIZE);
 	memset(disk, 0x0, BR_SIZE);
 	memcpy(disk + PARTITION_TABLE_OFFSET, part_table,
 		PARTITION_TABLE_SIZE);
 	*(unsigned short *)(disk + BR_SIGNATURE_OFFSET) = BR_SIGNATURE;
 }
-void copy_mbr_n_br(u8 *disk)
-{
+void copy_mbr_n_br(u8 *disk) {
 	int i;
 
 	copy_mbr(disk);
-	for (i = 0; i < ARRAY_SIZE(def_log_part_table); i++)
-	{
+	for (i = 0; i < ARRAY_SIZE(def_log_part_table); i++) {
 		copy_br(disk, def_log_part_br_abs_start_sector[i], &def_log_part_table[i]);
 	}
 }
 /* Structure associated with Block device*/
-struct mydiskdrive_dev 
-{
-	int size;
-	u8 *data;
+struct mydiskdrive_dev {
+	size_t size;
+	u8* data;
 	spinlock_t lock;
-	struct request_queue *queue;
-	struct gendisk *gd;
+	struct blk_mq_tag_set tag_set;
+	struct request_queue* queue;
+	struct gendisk* gd;
 
-}device;
+} device;
 
 struct mydiskdrive_dev *x;
 
@@ -189,7 +183,7 @@ static struct block_device_operations fops =
 
 int mydisk_init(void)
 {
-	(device.data) = vmalloc(MEMSIZE * SECTOR_SIZE);
+	(device.data) = vmalloc(MEMSIZE * MDISK_SECTOR_SIZE);
 	/* Setup its partition table */
 	copy_mbr_n_br(device.data);
 
@@ -216,12 +210,12 @@ static int rb_transfer(struct request *req)
 	rq_for_each_segment(bv, req, iter)
 	{
 		buffer = page_address(BV_PAGE(bv)) + BV_OFFSET(bv);
-		if (BV_LEN(bv) % (SECTOR_SIZE) != 0)
+		if (BV_LEN(bv) % (MDISK_SECTOR_SIZE) != 0)
 		{
 			printk(KERN_ERR"bio size is not a multiple ofsector size\n");
 			ret = -EIO;
 		}
-		sectors = BV_LEN(bv) / SECTOR_SIZE;
+		sectors = BV_LEN(bv) / MDISK_SECTOR_SIZE;
 		printk(KERN_DEBUG "my disk: Start Sector: %llu, Sector Offset: %llu;\
 		Buffer: %p; Length: %u sectors\n",\
 		(unsigned long long)(start_sector), (unsigned long long) \
@@ -229,13 +223,13 @@ static int rb_transfer(struct request *req)
 		
 		if (dir == WRITE) /* Write to the device */
 		{
-			memcpy((device.data)+((start_sector+sector_offset)*SECTOR_SIZE)\
-			,buffer,sectors*SECTOR_SIZE);		
+			memcpy((device.data)+((start_sector+sector_offset)*MDISK_SECTOR_SIZE)\
+			,buffer,sectors*MDISK_SECTOR_SIZE);		
 		}
 		else /* Read from the device */
 		{
 			memcpy(buffer,(device.data)+((start_sector+sector_offset)\
-			*SECTOR_SIZE),sectors*SECTOR_SIZE);	
+			*MDISK_SECTOR_SIZE),sectors*MDISK_SECTOR_SIZE);	
 		}
 		sector_offset += sectors;
 	}
@@ -248,26 +242,49 @@ static int rb_transfer(struct request *req)
 	return ret;
 }
 /** request handling function**/
+/**
+ * Changing according to blk multi-queue feature
+ */
 static void dev_request(struct request_queue *q)
 {
 	struct request *req;
 	int error;
-	while ((req = blk_fetch_request(q)) != NULL) /*check active request 
-						      *for data transfer*/
-	{
-	error=rb_transfer(req);// transfer the request for operation
-		__blk_end_request_all(req, error); // end the request
+	while ( (req = blk_fetch_request( q )) != NULL ) { /*check active request for data transfer */
+		error = rb_transfer( req ); // transfer the request for operation
+		__blk_end_request_all( req, error ); // end the request
 	}
 }
 
-void device_setup(void)
-{
+int device_setup(void) {
 	mydisk_init();
-	c = register_blkdev(c, "mydisk");// major no. allocation
-	printk(KERN_ALERT "Major Number is : %d",c);
-	spin_lock_init(&device.lock); // lock for queue
-	device.queue = blk_init_queue( dev_request, &device.lock); 
 
+	/* Register block device */
+	c = register_blkdev( c, "mydisk" );// major no. allocation
+	printk(KERN_ALERT "Major Number is : %d",c);
+
+	spin_lock_init( &device.lock ); // lock for queue
+
+	/* Initialize tag set */
+	device.tag_set.ops = &my_queue_ops;
+	device.tag_set.nr_hw_queues = 1;
+	device.tag_set.queue_depth = 128;
+	device.tag_set.numa_node = NUMA_NO_NODE;
+	device.tag_set.cmd_size = 0;
+	device.tag_set.flags = BLK_MQ_F_SHOULD_MERGE;
+	if ( blk_mq_alloc_tag_set( &(device.tag_set) ) )
+		return -ENOMEM;
+
+	/* Allocate queue */
+	device.queue = blk_mq_init_queue( &(device.tag_set) ); // blk_init_queue( dev_request, &device.lock);
+	if ( IS_ERR( device.queue ) ) {
+		blk_mq_free_tag_set( &(device.tag_set) );
+		return -ENOMEM;
+	} 
+
+	blk_queue_logical_block_size( device.queue, MDISK_SECTOR_SIZE );
+
+	/* Initialize gendisk */
+	// can't understand why we have here 8 minors
 	device.gd = alloc_disk(8); // gendisk allocation
 	
 	(device.gd)->major=c; // major no to gendisk
@@ -276,11 +293,17 @@ void device_setup(void)
 	device.gd->fops = &fops;
 	device.gd->private_data = &device;
 	device.gd->queue = device.queue;
-	device.size= mydisk_init();
+	device.size = mydisk_init();
 	printk(KERN_INFO"THIS IS DEVICE SIZE %d",device.size);	
-	sprintf(((device.gd)->disk_name), "mydisk");
-	set_capacity(device.gd, device.size);  
+	
+	/* Use buffer-safe functions */
+	snprintf( ((device.gd)->disk_name), DISK_NAME_LEN, "mydisk" );
+	
+	set_capacity(device.gd, device.size);
+
+	/* Achtung: after add_disk invokation it's possible to send request to disk */  
 	add_disk(device.gd);
+	return 0;
 }
 
 static int __init mydiskdrive_init(void)
@@ -300,8 +323,12 @@ void __exit mydiskdrive_exit(void)
 {	
 	del_gendisk(device.gd);
 	put_disk(device.gd);
-	blk_cleanup_queue(device.queue);
-	unregister_blkdev(c, "mydisk");
+
+	blk_mq_free_tag_set( &(device.tag_set) );
+
+	blk_cleanup_queue( device.queue );
+
+	unregister_blkdev( c, "mydisk" );
 	mydisk_cleanup();	
 }
 
